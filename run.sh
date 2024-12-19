cedf72a56292e8e6c107d67d8928037105f4b128
Chengsheng Zhang <935247090@qq.com>
2024-12-19 11:54:28 +0800

    CPU run success

--- run.sh
+++ run.sh
@@ -4,5 +4,8 @@ N=256
 T=8
 
 python3 RandGen.py $N
-nvcc life3d.cu -o life3d
-./life3d $N $T data/data.in data/data.out
+# nvcc life3d.cu -o life3d
+nvcc -std=c++17 life3d.cu -o life3d
+# ./life3d $N $T data/data.in data/data.out
+# life3d $N $T data/data.in data/data.out
+life3d 256 8 data/data.in data/data.out
